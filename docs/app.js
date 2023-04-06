542e803a3f96881f64d1283a8972f038453a46c7
Mohammed-Beltagy <justbeltagy@gmail.com>
2023-04-06 17:23:47 +0200

    Created Titles slider

--- docs/app.js
+++ docs/app.js
@@ -4,4 +4,4 @@ const main=document.querySelector("main");function repositionMain(){parseFloat(g
     `:main.style.cssText=`
       top: 50%
       transform: translate(-50%, -50%)
-    `}window.onload=repositionMain(),window.onresize=repositionMain();
+    `}window.onload=repositionMain(),window.onresize=repositionMain();const titlesSlider=document.querySelector(".titles .slider"),titles=Array.from(titlesSlider.children),leftArrow=document.getElementById("left-arrow"),rightArrow=document.getElementById("right-arrow");let active=0;function moveTitles(){titles.forEach(t=>t.classList.remove("active")),titles[active].classList.add("active");let e=0;for(let t=active-1;0<=t;t--)e+=parseFloat(getComputedStyle(titles[t]).getPropertyValue("width"));titlesSlider.style.left=-e+"px"}leftArrow.onclick=function(){0<active&&active--,moveTitles(),rightArrow.classList.remove("disabled"),active<=0?this.classList.add("disabled"):this.classList.remove("disabled")},rightArrow.onclick=function(){active<2&&active++,moveTitles(),leftArrow.classList.remove("disabled"),2<=active?this.classList.add("disabled"):this.classList.remove("disabled")};
--- stage/js/app.js
+++ stage/js/app.js
@@ -18,3 +18,41 @@ function repositionMain() {
 }
 window.onload = repositionMain();
 window.onresize = repositionMain();
+
+// Make Slider For The Titles
+const titlesSlider = document.querySelector(".titles .slider"),
+  titles = Array.from(titlesSlider.children),
+  leftArrow = document.getElementById("left-arrow"),
+  rightArrow = document.getElementById("right-arrow");
+let active = 0;
+leftArrow.onclick = function () {
+  if (active > 0) {
+    active--;
+  }
+  moveTitles();
+  rightArrow.classList.remove("disabled");
+  active <= 0
+    ? this.classList.add("disabled")
+    : this.classList.remove("disabled");
+};
+rightArrow.onclick = function () {
+  if (active < 2) {
+    active++;
+  }
+  moveTitles();
+  leftArrow.classList.remove("disabled");
+  active >= 2
+    ? this.classList.add("disabled")
+    : this.classList.remove("disabled");
+};
+function moveTitles() {
+  // Move (active) Class
+  titles.forEach((title) => title.classList.remove("active"));
+  titles[active].classList.add("active");
+  // Center The Title
+  let left = 0;
+  for (let i = active - 1; i >= 0; i--) {
+    left += parseFloat(getComputedStyle(titles[i]).getPropertyValue("width"));
+  }
+  titlesSlider.style.left = -left + "px";
+}
